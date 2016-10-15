2afeac493d6c95bf40189f316455040f9a7015f1
khaled <kaldmax@hotmail.com>
2016-10-15 04:34:48 +0300

    first

--- resources/views/media/phone.blade.php
+++ resources/views/media/phone.blade.php
@@ -1,31 +1,3 @@
-<?php
-
-function sluggable($str)
-{
-
-    $before = array(
-            'àáâãäåòóôõöøèéêëðçìíîïùúûüñšž',
-            '/[^a-z0-9\s]/',
-            array('/\s/', '/--+/', '/---+/')
-    );
-
-    $after = array(
-            'aaaaaaooooooeeeeeciiiiuuuunsz',
-            '',
-            '-'
-    );
-
-    $str = strtolower($str);
-    $str = strtr($str, $before[0], $after[0]);
-    $str = preg_replace($before[1], $after[1], $str);
-    $str = trim($str);
-    $str = preg_replace($before[2], $after[2], $str);
-
-    return $str;
-}
-
-?>
-
 <article class="col-md-4 col-sm-6 article">
     <div class="inner-article">
         <div class="article-img">
