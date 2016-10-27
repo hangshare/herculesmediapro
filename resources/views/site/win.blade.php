b794be6cfe53a38a7d9dc550c58f9a68c92e733b
khaled <kaldmax@hotmail.com>
2016-10-28 00:38:11 +0300

    first

--- resources/views/site/win.blade.php
+++ resources/views/site/win.blade.php
@@ -9,9 +9,22 @@
     <div class="container text-center" style="margin-top: 20px;">
         <p style="margin-top: 10px;">{{ trans('app.step1') }}</p>
         <p>{{ trans('app.step2') }}</p>
-        <a id="win" style="margin-top: 20px;" class="btn btn-danger btn-lg" href="#">{{ trans('app.registernow') }}</a>
+        {{--<a id="win" style="margin-top: 20px;" class="btn btn-danger btn-lg" href="#">{{ trans('app.registernow') }}</a>--}}
 
-        <div style="margin-top: 10px">
+        <a id="win" href="#" style="-moz-user-select: none;
+                                    background: #2a49a5 none repeat scroll 0 0;
+                                    border: 1px solid #082783;
+                                    box-shadow: 0 1px #4c6bc7 inset;
+                                    color: white;
+                                    font-size: 19px;
+                                    padding: 7px 22px;
+                                    text-decoration: none;
+                                    cursor: pointer;
+                                    position: relative;
+                                    top: 30px;">
+            <i class="fa fa-facebook"></i> | انشر مقالة على صفحة الفيس بوك واشترك بالسحب
+        </a>
+        <div style="margin-top: 70px">
             <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             <!-- responsive - new mobile upper post -->
             <ins class="adsbygoogle"
@@ -29,7 +42,8 @@
     <hr/>
     <div class="container">
         <div style="margin-top: 20px">
-            <h2><a title="{{ $post->title }}" href="https://www.hangshare.com/{{ $post->urlTitle }}/" target="_blank">{{ $post->title }}</a></h2>
+            <h2><a title="{{ $post->title }}" href="https://www.hangshare.com/{{ $post->urlTitle }}/"
+                   target="_blank">{{ $post->title }}</a></h2>
             {!!  $post->body->body !!}
         </div>
     </div>
