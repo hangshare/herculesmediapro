88b42e39c9f213e95d6fd92eb37d872a62cff657
khaled <kaldmax@hotmail.com>
2016-11-06 05:05:43 +0200

    first

---
+++ resources/views/user/request.blade.php
@@ -0,0 +1,9 @@
+@section('title', 'هرقل ميديا')
+@section('description', '')
+@extends('layouts.main')
+@section('content')
+    <div class="container mt-100 text-center" style="height: 600px">
+        <h1 style="font-size: 50px;">تم تسجيلك بالسحب على الهاتف</h1>
+        <p style="font-size: 30px;">في حال فوزك بالجائزة سوف يقوم فريق العمل بالتواصل معك من خلال البريد الاكتروني الخاص بك و الحصول على معلوماتك لايصال الهاتف اليك.</p>
+    </div>
+@endsection
